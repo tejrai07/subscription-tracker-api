40e7b177d0700b8fd156712be6ca815f4f847123
tejash rai <tejashrai045@gmail.com>
2026-02-16 19:12:23 +0530

    Fix variable namen

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const subscriptionController = require('../controllers/subscriptionController');
 
-// Routes
+const Subscription = require('../models/subscription'); 
+
+
 router.get('/', async (req, res) => {
   try {
-    const { userId } = req.query; 
+    const { userId } = req.query;
 
-    
     if (!userId) {
       return res.json([]); 
     }
 
-    // This is a comment to force a deployment
     
     const subscriptions = await Subscription.find({ userId });
     res.json(subscriptions);
@@ -20,18 +19,48 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
 router.post('/', async (req, res) => {
   try {
-    
-    const newSubscription = new Subscription(req.body);
+   
+    const newSubscription = new Subscription(req.body); 
     await newSubscription.save();
     res.status(201).json(newSubscription);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
-router.post('/:id/log', subscriptionController.logUsage);
-router.delete('/:id', subscriptionController.deleteSubscription);
 
-router.put('/:id', subscriptionController.updateSubscription);
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await Subscription.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedSubscription = await Subscription.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedSubscription);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+router.post('/:id/log', async (req, res) => {
+  try {
+    const sub = await Subscription.findById(req.params.id);
+    sub.usageHistory.push(new Date());
+    await sub.save();
+    res.json(sub);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 module.exports = router;
